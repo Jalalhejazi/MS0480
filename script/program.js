163ff6f36afe2cfd72db026d8bab1e7f93952924
Jalal Hejazi <jalal.hejazi@gmail.com>
2013-04-30 11:59:39 +0200

    program.js

---
+++ script/program.js
@@ -0,0 +1,43 @@
+function getCityFromZipCode(sZipcodeSelector, sTargetSelector) {
+  var sLoadingClass = 'loading';
+  var $sTargetSelector = jQuery(sTargetSelector);
+  var $sZipcodeSelector = jQuery(sZipcodeSelector);
+  $sTargetSelector.html("");
+
+  //var test = "test";
+
+  if ($sTargetSelector.length > 0 && $sZipcodeSelector.length > 0) {
+
+    var sZipCode = $sZipcodeSelector.val();
+    //DK post-nummer er en string med længden på 4
+    //For at undgå unødvendig $.ajax call til serveren.
+    if (sZipCode.length == 4) {
+      $sTargetSelector.removeClass(sLoadingClass).addClass(sLoadingClass);
+      $.ajax({
+        url: 'http://geo.oiorest.dk/postnumre/' + sZipCode + '.json?callback=?',
+        method: 'get',
+        dataType: 'json',
+        crossDomain: true,
+        success: function(data) {
+          $sTargetSelector.html(data.navn);
+          $sTargetSelector.removeClass(sLoadingClass);
+        }
+      });
+
+      return true;
+    }
+  } else {
+
+    return false;
+  }
+}
+
+
+$(function() {
+
+  $('#postNummer').focus();
+
+  $('#postNummer').on('keyup', function() {
+    getCityFromZipCode('#postNummer', '#byNavn');
+  });
+});
